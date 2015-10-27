b42fcde83e6deba71a08e707fb343a1b98bb97a3
garettarrowood <garettarrowood@gmail.com>
2015-10-27 12:58:39 -0400

    added validations and dependencies

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ActiveRecord::Base
-  has_many :students
-  has_many :separations
+  has_many :students, dependent: :delete_all
+  has_many :separations, dependent: :delete_all
   belongs_to :user
   
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,4 @@
 class Student < ActiveRecord::Base
   belongs_to :group
+  validates :first_name, :last_name, :gender, presence: true
 end
