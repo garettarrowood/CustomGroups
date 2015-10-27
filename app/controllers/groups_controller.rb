283d737a61f8bff6368fe86997f9ec08d388d113
garettarrowood <garettarrowood@gmail.com>
2015-10-27 08:56:37 -0400

    added view templates and lots of notes in controllers to guide build

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,39 @@
 class GroupsController < ApplicationController
+
+  def index
+    # From home page, you select a group or create a group
+  end
+
+  def show
+    # main class page - From “group” page, can view entire students roster, can click to add student (or adjust roster), (mass assignment, or click to add one more - either way, should be able to view class as you add), and can click to choose to generate random groups, can click "class settings" (or adjust roster)
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    # save group
+  end
+
+  def edit
+    # change group title
+  end
+
+  def update
+    # save new group title
+  end
+
+  def destroy
+    # delete group
+  end
+
+  def small_groups
+    # From random generation page, can specify how many groups you want, and whether they should be required to be mixed gender (exceptions always apply, but DO NOT remind user here) - SO THIS PAGE DISPLAYS FORM
+  end
+
+  def grouping
+    # takes #small_groups data and generates groups, renders a page with links back to other stuff
+  end
+
 end
--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,2 +1,32 @@
 class PeopleController < ApplicationController
+
+  def index
+    # shows class roster - Class settings - CRUD operations on students, can also add exceptions, should ALSO be able to view full roster here
+  end
+
+  def new
+    # single/mass assignment of student creation
+  end
+
+  def create
+    # save new student(s)
+  end
+
+  def exception
+    # explain how exceptions always apply to random generation - Exception selection form - dropdown menu to choose two students from same class
+  end
+
+  def add_exception
+    # save exception to group? save it to Exception class?
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
