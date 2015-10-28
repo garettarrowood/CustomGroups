16cb57daf89c2ef0a653860b577844d85e695b7e
garettarrowood <garettarrowood@gmail.com>
2015-10-28 15:48:22 -0400

    edited and deleted devise view templates, improved devise config

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+
+
+
+
+  # these email methods are here to over-ride built in email functionality in Devise. Without them, Devise would cause authentication errors.
   def email_required?
     false
   end
