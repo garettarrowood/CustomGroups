3ae9476ae1a7fd5237d875bad527e3c5cb05ccf1
garettarrowood <garettarrowood@gmail.com>
2015-10-30 08:56:03 -0400

    cleaning things up

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     get '/groups/class_edit', to: "groups#class_edit", as: 'class_edit'
     resources :groups do
       get 'students/roster_edit', to: "students#roster_edit"
-      resources :students
+      resources :students, except: [:index, :show, :edit]
     end
     get '/groups/:id/small_groups', to: "groups#small_groups", as: "small_groups"
     get '/groups/:id/class_settings', to: "groups#class_settings", as: "class_settings"
