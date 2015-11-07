0fc11f89bfef844f4681c18b8a236432ec2b46bb
garettarrowood <garettarrowood@gmail.com>
2015-11-07 11:23:38 -0500

    Clearer Randomizer and class_settings fix

--- app/models/randomizer.rb
+++ app/models/randomizer.rb
@@ -6,77 +6,85 @@ class Randomizer
       "" == @number ? @number = "3" : nil
     end
 
-    def find_balance
-      girls = @group.students.select do |student|
+    def find_gender_balance
+      @girls = @group.students.select do |student|
         "female" == student.gender
       end
-      boys = @group.students.select do |student|
+      @boys = @group.students.select do |student|
         "male" == student.gender  
       end
-      @minority = boys.length < girls.length ? boys : girls
-      @majority = boys.length >= girls.length ? boys : girls
-      @students = @majority.shuffle
     end
 
-    def full_names
-      @full_names = @students.collect do |student|
+    def minority
+      find_gender_balance
+      minority = @boys.length < @girls.length ? @boys : @girls
+      minority.shuffle
+    end
+
+    def majority
+      find_gender_balance
+      majority = @boys.length >= @girls.length ? @boys : @girls
+      majority.shuffle
+    end
+
+    def full_names(students)
+      students.collect do |student|
         "#{student.first_name} #{student.last_name}"
       end
     end
 
-    def spread_minority
-      @minority_names = @minority.shuffle.collect do |student|
-        "#{student.first_name} #{student.last_name}"
-      end
-      if @number.to_i < @minority_names.length
+    def spread_minority(students)
+      minority_names = full_names(students)
+      if @number.to_i < minority_names.length
         i=1
         @number.to_i.times do
-          @subgroups["#{i}"] << @minority_names[0]
-          @minority_names = @minority_names.drop(1)
+          @subgroups["#{i}"] << minority_names[0]
+          minority_names = minority_names.drop(1)
           i += 1
         end
-        @full_names << @minority_names
-        @full_names.flatten!
       else
         i=0
-        @minority_names.length.times do
-          @subgroups["#{@number.to_i - i}"] << @minority_names[0]
-          @minority_names = @minority_names.drop(1)
+        minority_names.length.times do
+          @subgroups["#{@number.to_i - i}"] << minority_names[0]
+          minority_names = minority_names.drop(1)
           i += 1
         end
       end
+      minority_names
     end    
 
-    def randomize_students
-      @students = @group.students.shuffle
+    def randomized_students
+      @group.students.shuffle
     end
 
-    def establish_subgroups
+    def establish_subgroups(students)
       @subgroups = {}
-      base_group_size = @full_names.length / @number.to_i
+      names_array = full_names(students)
+      base_group_size = names_array.length / @number.to_i
       i=1
       @number.to_i.times do
-        @subgroups["#{i}"] = @full_names[0...base_group_size]  
+        @subgroups["#{i}"] = names_array[0...base_group_size]  
         i += 1  
-        @full_names = @full_names.drop(base_group_size)
+        names_array = names_array.drop(base_group_size)
       end
+      names_array
     end
 
-    def distribute_one_more_iteration
+    def one_more_iteration(student_names)
       i=1
       @number.to_i.times do
-        @subgroups["#{i}"] << @full_names[0]
-        @full_names = @full_names.drop(1)
+        @subgroups["#{i}"] << student_names[0]
+        student_names = student_names.drop(1)
         i += 1
       end
+      @leftovers = student_names
     end
 
-    def distribute_leftovers
-      leftovers = @full_names.length
+    def distribute_leftovers(student_names)
       i=1
-      leftovers.times do
-        @subgroups["#{i}"] << @full_names[0]
-        @full_names = @full_names.drop(1)
+      student_names.length.times do
+        @subgroups["#{i}"] << student_names[0]
+        student_names = student_names.drop(1)
         i += 1
       end
     end
@@ -85,7 +93,7 @@ class Randomizer
       @group.separations != []
     end
 
-    def separator_checker
+    def separator_finder
       @group.separations.each do |separt|
         @subgroups.each do |group_number, student_array|
           if @subgroups[group_number].include?(separt.id1_to_name) && @subgroups[group_number].include?(separt.id2_to_name)
@@ -120,22 +128,17 @@ class Randomizer
     def sort
       number_check
       if "1" == @group.genderfied 
-        find_balance
-        full_names
-        establish_subgroups
-        spread_minority
-        until @full_names.length < @number.to_i do
-          distribute_one_more_iteration
+        @leftovers = establish_subgroups(majority) + spread_minority(minority)
+        until @leftovers.length < @number.to_i do
+          one_more_iteration(@leftovers)
         end
-        distribute_leftovers
+        distribute_leftovers(@leftovers)
       else
-        randomize_students
-        full_names
-        establish_subgroups
-        distribute_leftovers
+        leftovers = establish_subgroups(randomized_students)
+        distribute_leftovers(leftovers)
       end
       if separation_detector
-        until "pass" == separator_checker
+        until "pass" == separator_finder
           student_switcher 
         end
       end
@@ -143,7 +146,5 @@ class Randomizer
         @subgroups[group_number] = students_array.shuffle
       end
     end
-
-
   end
 end
