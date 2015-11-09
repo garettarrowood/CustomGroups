c7f1b0f3837655a709016590986681f643d776b8
garettarrowood <garettarrowood@gmail.com>
2015-11-09 18:29:08 -0500

    group_factory setup

--- spec/factories/group_facory.rb
+++ spec/factories/group_facory.rb
@@ -3,6 +3,7 @@ require 'faker'
 FactoryGirl.define do
   factory :group do
     title { Faker::Name.title }
-    genderfied { ["1", nil].sample }
+    genderfied { ["1", "0"].sample }
+    user
   end
 end
