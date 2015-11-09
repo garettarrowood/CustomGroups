fc36080921a0ffdc61d74a8558f93acb0e252a5d
garettarrowood <garettarrowood@gmail.com>
2015-11-09 11:36:56 -0500

    user only valid with username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :groups
+  validates :username, presence: true, uniqueness: true
 
   # these email methods are here to over-ride built in email functionality in Devise. Without them, Devise would cause authentication errors.
   def email_required?
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,46 @@
-require 'rails_helper.rb'
+require 'rails_helper'
 
 describe User, type: :model do
-  let(:user) { create :user }
+  describe "creation"  do 
 
-  it "is a test" do
-    expect(true).to eq(true)
+    it "needs a username" do
+      user = User.new(password: "example_password")
+
+      user.save
+      expect(user).to_not be_valid
+    end
+
+    it "needs a password" do
+      user = User.new(username: Faker::Name.name)
+
+      user.save
+      expect(user).to_not be_valid
+    end
+
+    it "needs a password of over 6 characters" do
+      user = User.new(username: Faker::Name.name, password: "short")
+
+      user.save
+      expect(user).to_not be_valid
+
+      user2 = User.new(username: Faker::Name.name, password: "long_enough")
+
+      user2.save
+      expect(user2).to be_valid
+    end
+  end
+
+  describe "authentication" do
+
+  end
+
+  describe "factory" do
+    let(:user) { create :user }
+
+    it "is valid" do
+      expect(user).to be_valid
+    end
   end
 end
+
+# let(:user) { create :user }
