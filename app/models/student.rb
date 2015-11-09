7ab7bffb1f60c838c65a6dabbed13d54ceee6163
garettarrowood <garettarrowood@gmail.com>
2015-11-09 18:41:34 -0500

    added group assoc validation

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,4 @@
 class Student < ActiveRecord::Base
   belongs_to :group
-  validates :first_name, :last_name, :gender, presence: true
+  validates :first_name, :last_name, :gender, :group, presence: true
 end
