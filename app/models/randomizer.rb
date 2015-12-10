8c5cbadf1d64a158b02560c458dd17af2ca4502f
Garett Arrowood <garettarrowood@gmail.com>
2015-12-10 07:57:08 -0500

    refactor "establish_subgroups" to use #group_by enumerable, modified key to be actual integers and not strings

--- app/models/randomizer.rb
+++ app/models/randomizer.rb
@@ -1,6 +1,6 @@
 class Randomizer
   class << self
-    attr_accessor :number, :group, :subgroups, :leftovers
+    attr_accessor :number, :group, :subgroups
 
     def full_names(students)
       students.collect do |student|
@@ -13,14 +13,14 @@ class Randomizer
       if @number.to_i < minority_names.length
         i=1
         @number.to_i.times do
-          @subgroups["#{i}"] << minority_names[0]
+          @subgroups[i] << minority_names[0]
           minority_names = minority_names.drop(1)
           i += 1
         end
       else
         i=0
         minority_names.length.times do
-          @subgroups["#{@number.to_i - i}"] << minority_names[0]
+          @subgroups[@subgroups.length-i] << minority_names[0]
           minority_names = minority_names.drop(1)
           i += 1
         end
@@ -29,32 +29,25 @@ class Randomizer
     end
 
     def establish_subgroups(students)
-      @subgroups = {}
       names_array = full_names(students)
-      base_group_size = names_array.length / @number.to_i
-      i=1
-      @number.to_i.times do
-        @subgroups["#{i}"] = names_array[0...base_group_size]  
-        i += 1  
-        names_array = names_array.drop(base_group_size)
-      end
-      names_array
+      @subgroups = names_array.group_by {|name| (names_array.index(name) % @number.to_i) + 1 }
+      []
     end
 
     def one_more_iteration(student_names)
       i=1
       @number.to_i.times do
-        @subgroups["#{i}"] << student_names[0]
+        @subgroups[i] << student_names[0]
         student_names = student_names.drop(1)
         i += 1
       end
-      @leftovers = student_names
+      student_names
     end
 
     def distribute_leftovers(student_names)
-      i=1
+      i=0
       student_names.length.times do
-        @subgroups["#{i}"] << student_names[0]
+        @subgroups[@subgroups.length - i] << student_names[0]
         student_names = student_names.drop(1)
         i += 1
       end
@@ -62,10 +55,10 @@ class Randomizer
     end
 
     def separator_finder(student_groups)
-      @group.separations.each do |separt|
+      @group.separations.each do |separation|
         student_groups.each do |group_number, student_array|
-          if student_groups[group_number].include?(separt.id1_to_name) && student_groups[group_number].include?(separt.id2_to_name)
-            return @switch_this = [group_number, [separt.id1_to_name, separt.id2_to_name]]
+          if student_groups[group_number].include?(separation.id1_to_name) && student_groups[group_number].include?(separation.id2_to_name)
+            return @switch_this = [group_number, [separation.id1_to_name, separation.id2_to_name]]
           end
         end
       end
@@ -75,15 +68,15 @@ class Randomizer
     def student_switcher(student_groups)
       if @group.separations.length == 3
         case @switch_this[0]
-        when "1" 
-          i=["2", "3"].sample
-        when "2"
-          i=["1", "3"].sample
+        when 1 
+          i=[2, 3].sample
+        when 2
+          i=[1, 3].sample
         else
-          i=["1", "2"].sample
+          i=[1, 2].sample
         end
       else 
-        @switch_this[0] == "1" ? i="2" : i="1"
+        @switch_this[0] == 1 ? i=2 : i=1
       end
       rand_student = @switch_this[1].sample
       student_groups[@switch_this[0]].delete(rand_student)
@@ -109,15 +102,15 @@ class Randomizer
     end
 
     def gender_mixed
-      @leftovers = establish_subgroups(@group.majority.shuffle) + spread_minority
-      until @leftovers.length < @number.to_i do
-        one_more_iteration(@leftovers)
+      leftovers = establish_subgroups(@group.majority.shuffle) + spread_minority
+      until leftovers.length < @number.to_i do
+        leftovers = one_more_iteration(leftovers)
       end
-      distribute_leftovers(@leftovers)
+      distribute_leftovers(leftovers)
     end
 
     def totally_random
-      distribute_leftovers(establish_subgroups(@group.randomized_students))
+      establish_subgroups(@group.randomized_students)
     end
 
     def sort
