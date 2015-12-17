6061052d4b2416420d0ceae2662994c0be8c2b17
Garett Arrowood <garettarrowood@gmail.com>
2015-12-17 11:55:05 -0500

    Better user comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         # also available are: :confirmable, :lockable, :timeoutable and :omniauthable
 
   has_many :groups
   validates :username, :password, presence: true
   validates :username, uniqueness: true
 
-  # these email methods are here to over-ride built in email functionality in Devise. Without them, Devise would cause authentication errors.
+  # These email methods are here to over-ride built in email functionality in Devise. 
+  # Without them, Devise would cause authentication errors.
   def email_required?
     false
   end
