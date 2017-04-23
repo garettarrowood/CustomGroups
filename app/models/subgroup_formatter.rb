cd9e92d476b7d03d1debbe39ddcd309d41e6e3b3
Garett Arrowood <garettarrowood@gmail.com>
2017-04-23 11:42:10 -0400

    Make student names easier to see

--- app/models/subgroup_formatter.rb
+++ app/models/subgroup_formatter.rb
@@ -7,11 +7,11 @@ class SubgroupFormatter
   def html
     content = ""
     i=1
-    @groupdata.size.times do 
+    @groupdata.size.times do
       content<<'<div class="col-xs-4">'
-      content<<"<h4><u>Group #{i}</u></h4>"
+      content<<"<h3 class='group'>Group #{i}</h3>"
       @groupdata[i].each do |name|
-        content<<"<p>#{name}</p>"
+        content<<"<p class='student-name'>#{name}</p>"
       end
       content<<'</div>'
       i+=1
